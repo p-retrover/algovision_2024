4268ca2a88ec833e09c07d8bd71e00d01018ae94
p-retrover <fossbyte@hotmail.com>
2024-11-04 17:00:30 +0530

    11th commit

--- problem1.py
+++ problem1.py
@@ -1,24 +1,34 @@
-no_of_sides = int(input())
-no_of_spaces = int((no_of_sides -2)/2) - 2
-repeat = int((no_of_sides - 3)/2)
+side_len = int(input())
+
+no_of_spaces = int((side_len -2)/2) - 2
 # print(no_of_spaces)
-if no_of_sides % 2 == 0 :
+
+if side_len % 2 == 0 :
+
     print("NO")
 
-elif no_of_sides == 5 : # bug
+elif side_len == 5 : # bug
+
     print("YES")
+
     # figure
-    for i in range(1,no_of_sides+1):
-        if i == 1 or i == no_of_sides or i == int(no_of_sides/2)+1:
-            print(no_of_sides*"*")
+
+    for i in range(1,side_len+1):
+        if i == 1 or i == side_len or i == int(side_len/2)+1: # for 1st, middle and last line
+            print(side_len*"*")
         else:
             print("* * *")
 
-elif no_of_sides % 2 != 0 :
+elif side_len % 2 != 0 :
+
     print("YES")
+
     # figure
-    for i in range(1,no_of_sides+1):
-        if i == 1 or i == no_of_sides or i == int(no_of_sides/2)+1:
-            print(no_of_sides*"*")
+
+    for i in range(1,side_len+1):
+        if i == 1 or i == side_len or i == int(side_len/2)+1:
+            print(side_len*"*")
         else:
+
             print("*", no_of_spaces*" ","*", no_of_spaces*" ", "*")
+
