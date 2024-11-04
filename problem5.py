975eb39f7a2b22376399844fbf681219759153c9
p-retrover <fossbyte@hotmail.com>
2024-11-04 15:27:15 +0530

    7th commit

--- problem5.py
+++ problem5.py
@@ -2,27 +2,39 @@ a= int(input())
 b= int(input())
 k = 0
 p = 0
-counter = 0
+k2b_counter = 0
+
 # increase p and increase by b*(10**p) 
+
 while (k <= b):
+
     if (k+ a*(10**p) <= b):
         k += a*(10**p)
         p += 1
-        counter+=1
+        k2b_counter+=1
+
     else:
         break
+
 
 # now keep p constant and increase by b*(10**p)
+
 p = p-1
+
 while (k <= b):
+
     if (k+ a*(10**p) <= b):
         k += a*(10**p)
-        counter+=1
+        k2b_counter+=1
+
     else:
         break
 
+
 # increase by 1
+
 while (k< b):
     k+=1
-    counter+=1
-print(counter)
+    k2b_counter+=1
+print(k2b_counter)
+
