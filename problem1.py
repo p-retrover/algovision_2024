178771307d470613a8e67dadadf1a21f595d400d
p-retrover <fossbyte@hotmail.com>
2024-11-04 13:21:16 +0530

    3rd commit

--- problem1.py
+++ problem1.py
@@ -1,33 +1,36 @@
 no_of_sides = int(input())
-if no_of_sides % 2 == 0 & (no_of_sides<=25 | no_of_sides>=4) :
+no_of_spaces = int((no_of_sides -2)/2) - 2
+repeat = int((no_of_sides - 3)/2)
+print(no_of_spaces)
+if no_of_sides % 2 == 0 :
     print("NO")
-elif no_of_sides == 5 :
+
+elif no_of_sides == 5 : # bug
     print("YES")
     # figure
     print(no_of_sides*"*")
 
-    for i in range(int((no_of_sides-3)/2)):
+    for i in range(repeat):
         print("* * *")
     
     print(no_of_sides*"*")
 
-    for i in range(int((no_of_sides-3)/2)):
+    for i in range(repeat):
         print("* * *")
-    
+
     print(no_of_sides*"*")
-elif no_of_sides % 2 != 0 & (no_of_sides<=25 | no_of_sides>=4):
+
+elif no_of_sides % 2 != 0 :
     print("YES")
     # figure
     print(no_of_sides*"*")
 
-    for i in range(int((no_of_sides-3)/2)):
-        print("*", int(((no_of_sides-3)/2)-2)*" ","*", int(((no_of_sides-3)/2)-2)*" ", "*")
-    
+    for i in range(repeat):
+        print("*", no_of_spaces*" ","*", no_of_spaces*" ", "*")
+
     print(no_of_sides*"*")
 
-    for i in range(int((no_of_sides-3)/2)):
-        print("*", int(((no_of_sides-3)/2)-2)*" ", "*", int(((no_of_sides-3)/2)-2)*" ", "*")
-    
+    for i in range(repeat):
+        print("*", no_of_spaces*" ", "*", no_of_spaces*" ", "*")
+
     print(no_of_sides*"*")
-
-
