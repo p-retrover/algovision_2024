536a3bcc366f3702548c33b5d2363c704dd2a517
p-retrover <fossbyte@hotmail.com>
2024-11-05 11:16:43 +0530

    14th commit

--- problem4.py
+++ problem4.py
@@ -1,17 +1,24 @@
 pin = input()
-if len(pin) == 4:
-    if pin == pin [1]*len(pin):
-        print("weak")
+
+def isWeak(num):
+    if num == num [1]*len(num):
+        return True
     else:
         first_counter = 0
         second_counter = 0
         for i in range(0,2): # checks for cyclic and consequitive in first 3
-            if (int(pin[i])+1)%10 == int(pin[i+1]):
+            if (int(num[i])+1)%10 == int(num[i+1]):
                 first_counter += 1
         for i in range(1,3): # checks for cyclic and consequitive in last 3
-            if (int(pin[i])+1)%10 == int(pin[i+1]):
+            if (int(num[i])+1)%10 == int(num[i+1]):
                 second_counter += 1
         if first_counter == 2 or second_counter == 2:
-            print("weak")
+            return True
         else:
-            print("strong")
+            return False
+
+if len(pin) == 4:
+    if isWeak(pin) == True:
+        print("weak")
+    else:
+        print("strong")
